50778c5a60ff04e33080865f64d9a6135bee4a70
Tomislav Adamic <tomislav.adamic@gmail.com>
2024-07-08 15:32:00 +0200

    build: Bumped version 0.6.3 --> 0.6.4

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ project = "flask-marshmallow-openapi"
 copyright = (
     ", ".join(str(y) for y in range(2023, datetime.now().year + 1)) + ", " + author
 )
-release = "0.6.3"
+release = "0.6.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
